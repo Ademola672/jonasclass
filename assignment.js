d656219743aaeb9b7ced5697a01dc7b5b82ac1ef
coutinho <segunademola313@gmail.com>
2023-04-17 10:14:59 +0100

    assignment

---
+++ assignment.js
@@ -0,0 +1,7 @@
+const continet = "Africa";
+const country = "Nigeria";
+const population = 200000000;
+
+console.log(continet);
+console.log(country);
+console.log(population);
--- script.js
+++ script.js
@@ -1,2 +1,53 @@
-let x = 10;
-const yellow = 4;
+/*let js = "amazing";
+
+console.log(40 + 8 + 23 - 10);
+
+console.log("jonas");
+console.log(23);
+
+let firstName = "jonas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
+console.log(myCurrentJob);*/
+
+// let year = "tyui";
+
+// console.log(year);
+// console.log(typeof year);
+
+// let biro = 2;
+
+// console.log(biro);
+// console.log(typeof biro);
+
+// let accepted = true;
+
+// console.log(accepted);
+// console.log(typeof accepted);
+
+// let pen;
+
+// console.log(pen);
+// console.log(typeof pen);
+
+// pen = 7;
+// console.log(typeof null);
+
+const now = 2098;
+const ageSegun = now - 1997;
+const ageTosin = now - 2002;
+
+console.log(ageSegun, ageTosin);
+console.log(ageTosin * 2, ageSegun / 10, 2 ** 3);
+// 2 **3 means 2 to the power of 3 = 2*2*2
+
+const middleName = "segun";
+const firstName = "Ademola";
+
+console.log(middleName + firstName);
