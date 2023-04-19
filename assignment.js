cc7af9a41862a7dfd6667e1136fb9d6d73e8d00d
coutinho <segunademola313@gmail.com>
2023-04-19 13:19:15 +0100

    correction

--- assignment.js
+++ assignment.js
@@ -29,15 +29,15 @@ johnBMI = console.log(johnWeight / (johnHeight + johnHeight));
 
 // correction
 
-// const massMark = 78;
-// const heightMark = 1.69;
-// const massJohn = 92;
-// const heightJohn = 1.95;
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
 
-const massMark = 95;
-const heightMark = 1.88;
-const massJohn = 85;
-const heightJohn = 1.76;
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
 
 const BMIMark = massMark / heightMark ** 2;
 const BMIJohn = massJohn / heightJohn ** 2;
@@ -50,7 +50,7 @@ const markHigherBMI = BMIMark > BMIJohn;
 console.log(BMIMark, BMIJohn, markHigherBMI);
 
 if (BMIMark > BMIJohn) {
-  console.log(`Mark's BMI (26.8) is higher than john's(27.4)`);
+  console.log(`Mark's BMI (${BMIMark}) is higher than john's(${BMIJohn})`);
 } else {
-  console.log(`John's BMI (27.4) is higher than Mark's (26.8)`);
+  console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})`);
 }
