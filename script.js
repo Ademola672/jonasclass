18a40207d07bc17b982fc79d5b207ec9ef5c8955
coutinho <segunademola313@gmail.com>
2023-05-08 08:45:19 +0100

    falsy

--- script.js
+++ script.js
@@ -122,15 +122,27 @@ console.log(typeof middleName + " " + firstName);
 
 // type conversion
 
-const inputYear = "1991";
-console.log(Number(inputYear));
-console.log(Number(inputYear) + 18);
+// const inputYear = "1991";
+// console.log(Number(inputYear));
+// console.log(Number(inputYear) + 18);
 
-console.log(Number("Jonas"));
-console.log(typeof NaN);
+// console.log(Number("Jonas"));
+// console.log(typeof NaN);
 
-console.log(String(23));
-console.log(String(23) + "rd");
+// console.log(String(23));
+// console.log(String(23) + "rd");
 
-//type coercion
-console.log("I am " + 23 + "years old");
+// //type coercion
+// console.log("I am " + 23 + "years old");
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("jonas"));
+console.log(Boolean({}));
+
+const money = 0;
+if (money) {
+  console.log("Dont't spend it all");
+} else {
+  console.log("you should get a job");
+}
