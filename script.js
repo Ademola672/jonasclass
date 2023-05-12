e9c08bc949a7223f2fcc17950feec4c5dd060497
coutinho <segunademola313@gmail.com>
2023-05-12 07:16:48 +0100

    ternary operator

--- script.js
+++ script.js
@@ -245,3 +245,23 @@ if (day === "monday") {
 } else {
   console.log("Not a valid day");
 }
+
+//conditional operator a.k.a ternary operator
+const age = 23;
+// age >= 18
+//   ? console.log("i like to drink wine")
+//   : console.log(" like to drink water");
+
+const drink = age >= 18 ? "wine" : "water";
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = "wine";
+} else {
+  drink2 = "water";
+}
+
+console.log(drink2);
+
+console.log(`i like to drink ${age >= 18 ? "wine" : "water"}`);
