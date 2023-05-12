31a6c59b3bffb6d20c15485583dc91f32c1485a6
coutinho <segunademola313@gmail.com>
2023-05-12 06:00:39 +0100

    switch statement

--- script.js
+++ script.js
@@ -203,3 +203,28 @@ if (hasDriversLicense && hasGoodVision && !isTired && !oldEnough) {
 } else {
   console.log(" someoneelse should drive ");
 }
+
+const day = "sunday";
+
+switch (day) {
+  case "monday":
+    console.log("Plan course structure");
+    console.log(" Go to coding meet up ");
+    break;
+  case "tuesday":
+    console.log("Prepare theory videos");
+    break;
+  case "wednesday":
+  case "thursday":
+    console.log("Write code examples");
+    break;
+  case "fridays":
+    console.log("Record videos");
+    break;
+  case "saturday":
+  case "sunday":
+    console.log("Enjoy the weekend");
+    break;
+  default:
+    console.log("Not a valid day");
+}
