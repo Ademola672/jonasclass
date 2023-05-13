d5a5c06ce8b8b129ec12312c288f6330424b7988
coutinho <segunademola313@gmail.com>
2023-05-13 06:59:37 +0100

    functions

--- fundamental2/script.js
+++ fundamental2/script.js
@@ -1,1 +1,31 @@
 "use strict";
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log("i can drive");
+
+// // const interface = "auto";
+
+// function
+
+function logger() {
+  console.log("My name is oluwasegun");
+}
+// calling/running or invoking the function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  //   console.log(apples, oranges);
+  const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
