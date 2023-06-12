73b1dc9b05380f58721337aecd050366813fda16
coutinho <segunademola313@gmail.com>
2023-06-12 16:01:51 +0100

    spread and rest

--- data structures and operators/script.js
+++ data structures and operators/script.js
@@ -31,8 +31,48 @@ const restaurant = {
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3} `
     );
   },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
 
+//SPREAD, because on right side of =
+const arr = [1, 2, ...[3, 4]];
+
+//REST,because on left side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [Pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+
+console.log(Pizza, risotto, otherFood);
+
+//objects///////////
+
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+// functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum = sum + numbers[i];
+  console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza("mushrooms", "onions", "olives", "spinach");
+restaurant.orderPizza("mushrooms");
 //spread operators//////////////////////////////////////
 // copy array
 const mainMenuCopy = [...restaurant.mainMenu];
