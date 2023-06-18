c194ed643a3c4329def6953ad2e6ead43af49686
coutinho <segunademola313@gmail.com>
2023-06-18 19:25:32 +0100

    rounding up my assignment

--- data structures and operators/assignment.js
+++ data structures and operators/assignment.js
@@ -94,3 +94,21 @@ const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
 console.log(players1Final);
 
 //5
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+//6
+let printGoals = function (...players) {
+  console.log(players);
+  console.log(`${players.length} goals were scored`);
+};
+
+printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
+printGoals("Davies", "Muller");
+printGoals(...game.scored);
+
+//7
+team1 < team2 && console.log(" Team 1 is likely to win ");
+team1 > team2 && console.log(" Team 1 is likely to win ");
