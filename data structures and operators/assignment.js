8aa5046dedefceb9c5874700655a6c3cf04e54ec
coutinho <segunademola313@gmail.com>
2023-06-21 13:17:27 +0100

    assignment and correction done

--- data structures and operators/assignment.js
+++ data structures and operators/assignment.js
@@ -249,27 +249,24 @@ HINT 4: This challenge is difficult on purpose, so start watching the solution i
 Afterwards, test with your own test data!
 
 GOOD LUCK 😀
-*/
 
-const textArea = document.body.append(document.createElement("textarea"));
-const button = document.body.append(document.createElement("button"));
+
+document.body.append(document.createElement("textarea"));
+document.body.append(document.createElement("button"));
 
 document.querySelector("button").addEventListener("click", function () {
   const text = document.querySelector("textarea").value;
-  console.log(text);
-});
-// const conversion = function (underScore) {
-//   const underScore = "";
-//   const underScoreLower = "".toLowerCase();
-// };
+  const rows = text.split("\n");
+  // console.log(rows);
 
-button.getEv("clicked", function (underScore) {
-  const underScoreLower = underScore.toLowerCase().delete("_");
-  console.log(underScoreLower);
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split("_");
+    // console.log(first, second);
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )} `;
+    console.log(`${output.padEnd(20)}${"✅".repeat(i + 1)}`);
+  }
 });
-
-conversion(""); //underscore_case
-conversion(""); //first_name
-conversion(""); //Some_Variable
-conversion(""); //calculate_AGE
-conversion(""); //delayed_departure
+*/
