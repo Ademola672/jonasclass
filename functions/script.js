4644ea2bfe80e740eaefd65e98b2002b6dc8e524
coutinho <segunademola313@gmail.com>
2023-06-22 06:08:44 +0100

    Functions Accepting Callback Functions

--- functions/script.js
+++ functions/script.js
@@ -26,7 +26,7 @@ createBooking("LH123", 2);
 createBooking("LH123", 5);
 
 createBooking("LH123", undefined, 1000);
-*/
+
 
 const flight = "LM234";
 const jonas = {
@@ -58,3 +58,31 @@ const newPassport = function (person) {
 
 newPassport(jonas);
 checkIn(flight, jonas);
+*/
+
+const oneWord = function (str) {
+  return str.replace(/ /g, "").toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+};
+
+//Higher-order functions
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Trandsformed by: ${fn.name}`);
+};
+
+transformer("JavaScript is the best!", upperFirstWord);
+transformer("JavaScript is the best!", oneWord);
+
+const high5 = function () {
+  console.log("👋");
+};
+//JS uses callbacks all the time
+// document.body.addEventListener("click", high5);
+["Jonas", "Martha", "Adam"].forEach(high5);
