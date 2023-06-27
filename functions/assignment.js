dc856cba6dad50b715a0b2b801a1d183b903865a
coutinho <segunademola313@gmail.com>
2023-06-27 11:44:52 +0100

    advance functions assignment completed

--- functions/assignment.js
+++ functions/assignment.js
@@ -48,6 +48,28 @@ const poll = {
     );
     console.log(answer);
     //Register
+    typeof answer === "number" &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults("string");
+  },
+  displayResults(type = "array") {
+    if (type === "array") {
+      console.log(this.answers);
+    } else if (type === "string") {
+      console.log(`Poll results are ${this.answers.join(", ")}`);
+    }
   },
 };
-poll.registerNewAnswer();
+// poll.registerNewAnswer();
+
+document
+  .querySelector(".poll")
+  .addEventListener("click", poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] }, "string");
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, "string");
+// [5, 2, 3]
+// [1, 5, 3, 9, 6, 1]
