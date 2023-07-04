05f1e1cc0cf5edd9628dc1a84989ddfb1bc76359
coutinho <segunademola313@gmail.com>
2023-07-04 12:07:20 +0100

    Looping Arrays: forEach

--- working with arrays/script.js
+++ working with arrays/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ["GBP", "Pound sterling"],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 /*
@@ -105,7 +105,7 @@ console.log(...arr, ...arr2);
 
 //JOIN
 console.log(letters.join("-"));
-*/
+
 const arr = [23, 11, 64];
 console.log(arr[0]);
 console.log(arr.at(0));
@@ -118,3 +118,23 @@ console.log(arr.at(-1));
 // at method also work on string
 console.log("Jonas".at(0));
 console.log("Jonas".at(-1));
+*/
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements)
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+console.log("-----forEach--------");
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
