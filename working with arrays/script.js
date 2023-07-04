23d0cd29981d5d07cc1e90523921e40ebcecc982
coutinho <segunademola313@gmail.com>
2023-07-04 20:55:28 +0100

    The map Method

--- working with arrays/script.js
+++ working with arrays/script.js
@@ -176,3 +176,27 @@ currenciesUnique.forEach(function (value, _, map) {
   console.log(`${value}: ${value}`);
 });
 */
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUsd = 1.1;
+//const movementsUsd = movements.map(function (mov) {
+// return mov * eurToUsd;
+// });
+const movementsUsd = movements.map((mov) => mov * eurToUsd);
+
+console.log(movements);
+console.log(movementsUsd);
+
+const movementsUsdFor = [];
+for (const mov of movements) movementsUsdFor.push(mov * eurToUsd);
+
+console.log(movementsUsdFor);
+
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? "deposited" : "withdrew"} ${Math.abs(
+      mov
+    )}`
+);
+
+console.log(movementsDescriptions);
