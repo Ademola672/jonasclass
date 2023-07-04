243ebfe76158dc4d94cd645eb39b053c8344ea06
coutinho <segunademola313@gmail.com>
2023-07-04 10:30:41 +0100

    closures assignment

--- functions/assignment.js
+++ functions/assignment.js
@@ -73,4 +73,14 @@ poll.displayResults.call({ answers: [5, 2, 3] }, "string");
 poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, "string");
 // [5, 2, 3]
 // [1, 5, 3, 9, 6, 1]
+
+
+(function () {
+  const header = document.querySelector("h1");
+  header.style.color = "red";
+
+  document.querySelector("body").addEventListener("click", function () {
+    header.style.color = "blue";
+  });
+})();
 */
