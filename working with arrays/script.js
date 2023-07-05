37af72993bb90704a3bf9027c7840144c02b5768
coutinho <segunademola313@gmail.com>
2023-07-05 09:38:57 +0100

    The filter Method

--- working with arrays/script.js
+++ working with arrays/script.js
@@ -214,3 +214,19 @@ const movementsDescriptions = movements.map(
 
 console.log(movementsDescriptions);
 */
+
+//Filter
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const deposits = movements.filter((mov) => mov > 0);
+
+console.log(movements);
+console.log(deposits);
+// for of loop
+
+// const depositsFor = [];
+// for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+// console.log(depositsFor);
+//for withdrawals
+const withdrawals = movements.filter((mov) => mov < 0);
+console.log(withdrawals);
