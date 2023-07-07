431d9985dd4ce5c1ef89a716a67f1734c88a7b71
coutinho <segunademola313@gmail.com>
2023-07-07 22:10:20 +0100

    The findIndex Method

--- working with arrays/script.js
+++ working with arrays/script.js
@@ -191,6 +191,29 @@ btnTransfer.addEventListener("click", function (e) {
     updateUI(currentAccount);
   }
 });
+
+btnClose.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("Delete");
+  if (
+    inputCloseUsername.vlue === currentAccount.value &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username
+    );
+
+    console.log(index);
+
+    //Delete account
+
+    accounts.splice(index, 1);
+
+    //Hide UI
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputClosePin.value = "";
+});
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
