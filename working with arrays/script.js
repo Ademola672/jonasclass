f45d432d037f5a3e1f124c71dcd0191e615582e0
coutinho <segunademola313@gmail.com>
2023-07-08 15:43:03 +0100

    More Ways of Creating and Filling Arrays

--- working with arrays/script.js
+++ working with arrays/script.js
@@ -473,7 +473,7 @@ const overallBalance2 = accounts
   .flatMap((acc) => acc.movements)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(overallBalance);
-*/
+
 
 //Strings
 const owners = ["Jonas", "Zach", "Adam", "Martha"];
@@ -499,3 +499,38 @@ console.log(movements);
 
 movements.sort((a, b) => b - a);
 console.log(movements);
+*/
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+//Empty arrays + fill
+const x = Array(7);
+console.log(x);
+// console.log(x.map() => 5);
+x.fill(1, 3, 5);
+x.fill(1);
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+//Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+const dice = Array.from({ length: 100 }, (_, i) => Math.random(i + 1));
+console.log(dice);
+
+labelBalance.addEventListener("click", function () {
+  const movementsUi = Array.from(
+    document.querySelectorAll(".movements__value"),
+    (el) => Number(el.textContent.replace("EUR", ""))
+  );
+  // console.log(movementsUi);
+  console.log(movementsUi);
+
+  const movementsUi2 = [...document.querySelectorAll(".movements__value")];
+});
