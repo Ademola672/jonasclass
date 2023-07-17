4d185da6beee3bb52d0e42c61c609aeaf3012592
coutinho <segunademola313@gmail.com>
2023-07-17 12:54:54 +0100

    Numeric Separators

--- Numbers-Dates-Timers-Bankist/script.js
+++ Numbers-Dates-Timers-Bankist/script.js
@@ -333,7 +333,7 @@ console.log((2.7).toFixed(0));
 console.log((2.7).toFixed(3));
 console.log((2.345).toFixed(2));
 console.log(+(2.345).toFixed(2));
-*/
+
 
 console.log(5 % 2);
 console.log(5 / 2); // 5 = 2 + 2 + 1
@@ -360,3 +360,19 @@ labelBalance.addEventListener("click", function () {
     if (i % 3 === 0) row.style.backgroundColor = "blue";
   });
 });
+*/
+// 287,000,000
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const priceCents = 345_99;
+console.log(priceCents);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.1415;
+console.log(PI);
+
+console.log(Number("230_000"));
+console.log(parseInt("230_000"));
