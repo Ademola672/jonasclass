9ebffdb9760af42c1f02ab6bb80c97af22600e66
coutinho <segunademola313@gmail.com>
2023-07-23 05:29:34 +0100

    Event Delegation: Implementing Page Navigation

--- Advanced-Dom-Bankist/script.js
+++ Advanced-Dom-Bankist/script.js
@@ -7,6 +7,11 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+/////////////////////////////////////
+//Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -32,6 +37,65 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+/////////////////////////////////////////////////////////////////
+//Button Scrollig
+
+btnScrollTo.addEventListener("click", function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    "height/width viewport",
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  //scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior: "smooth",
+  });
+
+  section1.scrollIntoView({ behavior: "smooth" });
+});
+
+/////////////////////////////////////////////////////////////////////////////////
+//Page navigation
+// document.querySelectorAll(".nav__link").forEach(function (el) {
+//   el.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute("href");
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//   });
+// });
+
+// 1. Add event listener to common prent element
+// 2. Determine what element originaated the event 
+
+document.querySelector('.nav__link').addEventListener('click', function(e) {
+  e.preventDefault()
+
+  //Matching strtegy
+  if(e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href')
+    console.log(id);
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+  }
+
+}), 
+
 /////////////////////////////////////////////////
 ///////////////////////////////////////
 /*
@@ -118,37 +182,7 @@ logo.classList.contains("c"); //not includes
 //Dont use
 logo.classList = "jonas";
 
-const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section--1");
-
-btnScrollTo.addEventListener("click", function (e) {
-  const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
-
-  console.log(e.target.getBoundingClientRect());
-
-  console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
-
-  console.log(
-    "height/width viewport",
-    document.documentElement.clientHeight,
-    document.documentElement.clientWidth
-  );
-
-  //scrolling
-  // window.scrollTo(
-  //   s1coords.left + window.pageXOffset,
-  //   s1coords.top + window.pageYOffset
-  // );
-
-  window.scrollTo({
-    left: s1coords.left + window.pageXOffset,
-    top: s1coords.top + window.pageYOffset,
-    behavior: "smooth",
-  });
-
-  section1.scrollIntoView({ behavior: "smooth" });
-});
+//Types of events and event handlers
 
 const h1 = document.querySelector("h1");
 
