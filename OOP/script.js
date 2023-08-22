8b8a9154cc694d0c66e8e43c56fba6531f78ef86
coutinho <segunademola313@gmail.com>
2023-08-22 09:56:49 +0100

    Another Class Example

--- OOP/script.js
+++ OOP/script.js
@@ -286,7 +286,7 @@ class StudentCl extends PersonCl {
 const martha = new StudentCl("Martha Jones", 2012, "Computer Science");
 martha.introduce();
 martha.calcAge();
-*/
+
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 //Inheritance Between "Classes : Objects.create"
@@ -318,3 +318,43 @@ const jay = Object.create(StudentProto);
 jay.init("jay", 2010, "Computer Science");
 jay.introduce();
 jay.calcAge();
+*/
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account ${this.owner}`);
+  }
+
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log("Loan approved");
+    }
+  }
+}
+
+const acc1 = new Account("Jonas", "EUR", 1111);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+
+console.log(acc1);
