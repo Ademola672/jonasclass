c27024e697d07f758ee902f3800f77678126f06c
coutinho <segunademola313@gmail.com>
2023-08-23 14:09:38 +0100

    Coding Challenge #4

--- OOP/assignment.js
+++ OOP/assignment.js
@@ -136,7 +136,7 @@ calling 'accelerate', 'brake' and 'chargeBattery'
 DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
 
 GOOD LUCK 😀
-*/
+
 
 const Car = function (brand, speed) {
   this.brand = brand;
@@ -178,3 +178,87 @@ tesla.chargeBattery(90);
 tesla.brake();
 tesla.accelerate();
 console.log(tesla);
+*/
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+1. Re-create challenge #3, but this time using 
+ES6 classes: create an 'EVCl' child class of
+ the 'CarCl' class
+2. Make the 'charge' property private;
+3. Implement the ability to chain the 'accelerate' 
+and 'chargeBattery' methods of this class, and also
+ update the 'brake' method in the 'CarCl' class. 
+ They experiment with chining!
+
+DATA CAR 1: 'Rivian' going at 120 km/h, with a
+ charge of 23%
+
+GOOD LUCK 😀
+*/
+
+class CarCl {
+  constructor(brand, speed) {
+    this.brand = brand;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed = this.speed + 10;
+    console.log(`${this.brand} is going at ${this.speed}Km/h`);
+  }
+
+  brake() {
+    this.speed = this.speed - 5;
+    console.log(`${this.brand} is going at ${this.speed}Km/h`);
+    return this;
+  }
+
+  get speedUs() {
+    return this.speed / 1.6;
+  }
+
+  set speedUs(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+class EvCl extends CarCl {
+  #charge;
+  constructor(brand, speed, charge) {
+    super(brand, speed);
+    this.#charge = charge;
+  }
+
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+
+  accelerate() {
+    this.speed = this.speed + 20;
+    this.#charge--;
+
+    console.log(
+      `${this.brand} going at ${this.speed}Km/h, with a charge of ${
+        this.#charge
+      }%`
+    );
+    return this;
+  }
+}
+
+const rivian = new EvCl("Rivian", 120, 23);
+console.log(rivian);
+
+rivian
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .brake()
+  .chargeBattery(50)
+  .accelerate();
+
+console.log(rivian.speedUs);
